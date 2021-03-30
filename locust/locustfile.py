b3edc47d5d853e01907a597d4be8db549c4b606a
Antoine Rebillard <antoine.rebillard@pm.me>
2021-03-30 15:17:53 +0200

    added request to lucustfile

---
+++ locust/locustfile.py
@@ -0,0 +1,27 @@
+import time
+from locust import HttpUser, task, between
+
+
+class QuickstartUser(HttpUser):
+
+    wait_time = between(1, 2.5)
+
+    @task
+    def hello_world(self):
+        self.client.get("/hello")
+
+    @task
+    def show_summary(self):
+        self.client.get("/show_summary")
+
+    @task
+    def book(self):
+        self.client.get("/book/Spring%2520Festival/Simply%2520Lift")
+
+    @task
+    def book(self):
+        self.client.get("/points")
+
+    def on_start(self):
+        self.client.post("/", data={"email": "john@simplylift.co"})
+    
